3ecdfb141b659598d90750d01c8613d81b6da34b
Joe Scott <jbs198585@gmail.com>
2022-06-30 07:19:24 -0400

    Express session

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
 
+app.use(session({ secret: 'thisisnotagoodsecret' }));
+
+app.get('/viewcount', (req, res) => {
+  if (req.session.count) {
+    req.session.count += 1;
+  } else {
+    req.session.count = 1;
+  }
+  res.send(`You hav viewed this page ${req.session.count} times`)
+});
 
 app.listen(3000, () => {
   console.log('listening on port 3000')
